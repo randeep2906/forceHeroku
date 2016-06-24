c215703ec2cdd76230b0e23c17da0c6cfd355528
Randeep Jassal <randeepj@phaneendrap-m2.corp.intusurg.com>
2016-06-24 14:44:10 -0700

    Added index.php trigger

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("landingpage.html"); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+?>
